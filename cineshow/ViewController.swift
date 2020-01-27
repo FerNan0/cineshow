42a386773b563b46f50eaad1f1da2d5b1f92c80a
Fernando Ferreira Duarte <fernando.duarte@zup.com.br>
2020-01-27 19:27:42 -0300

    creating project

---
+++ cineshow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cineshow
+//
+//  Created by Fernando Duarte on 27/01/20.
+//  Copyright © 2020 Fernando Duarte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
